1535e61713ac0ee5c933bfe3863b0628a220784d
beha.frigui <mohamedbehaeddine.frigui@esprit.tn>
2019-09-12 15:13:35 +0200

    last

--- front/test.php
+++ front/test.php
@@ -1,13 +1,23 @@
 <?php
 
 function arabicTOen($str) {
-    $arabic_eastern = array('٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩');
-	$engNumber = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
-	return str_replace($arabic_eastern, $engNumber, $str);
+    $trans = array(
+        "&#1632;" => "0",
+        "&#1633;" => "1",
+        "&#1634;" => "2",
+        "&#1635;" => "3",
+        "&#1636;" => "4",
+        "&#1637;" => "5",
+        "&#1638;" => "6",
+        "&#1639;" => "7",
+        "&#1640;" => "8",
+        "&#1641;" => "9",
+    );
+    return strtr($str, $trans);
 }
 
 
-function numberToRomanRepresentation($number) {
+function engToRoman($number) {
     $map = array('M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400,
      'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 
      'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1);
@@ -25,11 +35,14 @@ function numberToRomanRepresentation($number) {
 }
 
 if(isset($_POST['submit'])){
-    $number=$_POST["faNumber"];
-    $engNumber = arabicTOen($_POST["faNumber"]);
+    $numberStr=(string)$_POST["faNumber"];
+    $engNumber = arabicTOen((string)$numberStr);
+    $romanNumber = engToRoman($engNumber);
+    echo "In Faris:".$numberStr."<br>";
+    echo "In Arabic:".$engNumber."<br>";
+    echo "In Roman:".$romanNumber."";
     
-        echo "<p>---".$engNumber."</p>";
-        echo $number;
+        
     
 }
 
